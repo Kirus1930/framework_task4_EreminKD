d3f799afa4388a3cad54f3676579a14030fd0859
Kirus1930 <k_eryomin00@mail.ru>
2026-04-20 11:26:49 +0300

    Create BookingController.cs

---
+++ BookingService/Controllers/BookingController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingService.Controllers;
+
+[ApiController]
+[Route("booking")]
+public class BookingController : ControllerBase
+{
+    private readonly Services.BookingService _service;
+
+    public BookingController(Services.BookingService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Process(
+        [FromHeader(Name = "X-Correlation-Id")] string correlationId,
+        [FromHeader(Name = "Idempotency-Key")] string idempotencyKey,
+        [FromQuery] string processId,
+        [FromQuery] string action)
+    {
+        await _service.HandleEvent(processId, idempotencyKey, correlationId, action);
+
+        return Ok();
+    }
+}
