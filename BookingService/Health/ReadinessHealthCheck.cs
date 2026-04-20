5e4d9f805867352e5df699d7b2a2ce0ee426eadc
Kirus1930 <k_eryomin00@mail.ru>
2026-04-20 11:27:50 +0300

    Create ReadinessHealthCheck.cs

---
+++ BookingService/Health/ReadinessHealthCheck.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace BookingService.Health;
+
+public class ReadinessHealthCheck : IHealthCheck
+{
+    private static int _failures = 0;
+
+    public static void RegisterFailure()
+    {
+        _failures++;
+    }
+
+    public Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+    {
+        if (_failures > 5)
+            return Task.FromResult(HealthCheckResult.Unhealthy("Too many failures"));
+
+        return Task.FromResult(HealthCheckResult.Healthy());
+    }
+}
