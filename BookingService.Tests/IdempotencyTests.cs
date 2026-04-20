65d23fb60d68994a72f126cd6c41a3f591ee1f0b
Kirus1930 <k_eryomin00@mail.ru>
2026-04-20 11:50:45 +0300

    Create IdempotencyTests.cs

---
+++ BookingService.Tests/IdempotencyTests.cs
@@ -0,0 +1,25 @@
+using BookingService.Domain;
+using BookingService.Infrastructure;
+using BookingService.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+public class IdempotencyTests
+{
+    [Fact]
+    public async Task Duplicate_Event_Should_Not_Change_State()
+    {
+        var repo = new InMemoryProcessRepository();
+        var logger = new Mock<ILogger<BookingService>>();
+        var service = new BookingService(repo, logger.Object);
+
+        await service.HandleEvent("1", "dup", "c1", "create");
+        await service.HandleEvent("1", "dup", "c1", "create");
+
+        var process = repo.GetOrCreate("1");
+
+        Assert.Equal(BookingState.Created, process.State);
+        Assert.Single(process.ProcessedEvents);
+    }
+}
