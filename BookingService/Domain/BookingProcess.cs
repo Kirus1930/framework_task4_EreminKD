bb9553ddc7826ac8a39f1247110fc59be742839e
Kirus1930 <k_eryomin00@mail.ru>
2026-04-20 11:18:21 +0300

    Create BookingProcess.cs

---
+++ BookingService/Domain/BookingProcess.cs
@@ -0,0 +1,11 @@
+namespace BookingService.Domain;
+
+public class BookingProcess
+{
+    public string ProcessId { get; set; } = default!;
+    public BookingState State { get; set; } = BookingState.None;
+
+    public HashSet<string> ProcessedEvents { get; set; } = new();
+
+    public DateTime LastUpdated { get; set; }
+}
