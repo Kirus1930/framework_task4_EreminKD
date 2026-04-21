482fff508c5bb08679a66263cc9bae7230844eac
Kirus1930 <k_eryomin00@mail.ru>
2026-04-21 08:46:07 +0300

    Update BookingService.AssemblyInfo.cs

--- BookingService/obj/Debug/net8.0/BookingService.AssemblyInfo.cs
+++ BookingService/obj/Debug/net8.0/BookingService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beea736624096b593bf007f3f981fa1fc28bb8e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c9eb4d23951bd19f8c300e7ff15cf15eaf6299")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
